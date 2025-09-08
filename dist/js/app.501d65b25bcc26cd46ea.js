4a526287488fb133202619e5b6ba7cfb7d7889f3
marvhock <marvhock@users.noreply.github.com>
2025-09-08 23:00:16 +0200

    New manual Release - Redesign 2025

---
+++ dist/js/app.501d65b25bcc26cd46ea.js
@@ -0,0 +1,108 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/css/main.scss":
+/*!***************************!*\
+  !*** ./src/css/main.scss ***!
+  \***************************/
+/***/ (() => {
+
+eval("{throw new Error(\"Module build failed (from ./node_modules/mini-css-extract-plugin/dist/loader.js):\\nHookWebpackError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nexpected \\\"(\\\".\\n\\u001b[34m  ╷\\u001b[0m\\n\\u001b[34m9 │\\u001b[0m   @media (min-width: (settings.breakpoint-sm\\u001b[31m\\u001b[0m)) {\\n\\u001b[34m  │\\u001b[0m \\u001b[31m                                            ^\\u001b[0m\\n\\u001b[34m  ╵\\u001b[0m\\n  src/css/components/base.scss 9:45  @use\\n  src/css/main.scss 5:1              root stylesheet\\n    at tryRunOrWebpackError (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/HookWebpackError.js:87:9)\\n    at __webpack_require_module__ (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5498:12)\\n    at __webpack_require__ (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5445:18)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5533:20\\n    at symbolIterator (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3485:9)\\n    at done (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (/Users/marvin/code/marvhock.github.io/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (/Users/marvin/code/marvhock.github.io/node_modules/tapable/lib/Hook.js:20:14)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5420:43\\n    at symbolIterator (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3482:9)\\n    at timesSync (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:2297:7)\\n    at Object.eachLimit (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3463:5)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5382:16\\n    at symbolIterator (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3485:9)\\n    at timesSync (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:2297:7)\\n    at Object.eachLimit (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3463:5)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5350:15\\n    at symbolIterator (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3485:9)\\n    at done (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3527:9)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5296:8\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:3696:6\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/HookWebpackError.js:68:2\\n    at Hook.eval [as callAsync] (eval at create (/Users/marvin/code/marvhock.github.io/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Cache.store (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Cache.js:113:20)\\n    at ItemCacheFacade.store (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/CacheFacade.js:142:15)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:3695:11\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Cache.js:97:34\\n    at Array.<anonymous> (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/cache/MemoryCachePlugin.js:46:13)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Cache.js:97:19\\n    at Hook.eval [as callAsync] (eval at create (/Users/marvin/code/marvhock.github.io/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:19:1)\\n    at Cache.get (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Cache.js:81:18)\\n    at ItemCacheFacade.get (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/CacheFacade.js:116:15)\\n    at Compilation._codeGenerationModule (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:3663:9)\\n    at codeGen (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5284:11)\\n    at symbolIterator (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3482:9)\\n    at timesSync (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:2297:7)\\n    at Object.eachLimit (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3463:5)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5314:14\\n    at processQueue (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/util/processAsyncTree.js:61:4)\\n    at process.processTicksAndRejections (node:internal/process/task_queues:85:11)\\n-- inner error --\\nError: Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\nexpected \\\"(\\\".\\n\\u001b[34m  ╷\\u001b[0m\\n\\u001b[34m9 │\\u001b[0m   @media (min-width: (settings.breakpoint-sm\\u001b[31m\\u001b[0m)) {\\n\\u001b[34m  │\\u001b[0m \\u001b[31m                                            ^\\u001b[0m\\n\\u001b[34m  ╵\\u001b[0m\\n  src/css/components/base.scss 9:45  @use\\n  src/css/main.scss 5:1              root stylesheet\\n    at Object.<anonymous> (/Users/marvin/code/marvhock.github.io/node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[1].use[1]!/Users/marvin/code/marvhock.github.io/node_modules/postcss-loader/dist/cjs.js!/Users/marvin/code/marvhock.github.io/node_modules/sass-loader/dist/cjs.js!/Users/marvin/code/marvhock.github.io/src/css/main.scss:1:7)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/javascript/JavascriptModulesPlugin.js:519:10\\n    at Hook.eval [as call] (eval at create (/Users/marvin/code/marvhock.github.io/node_modules/tapable/lib/HookCodeFactory.js:19:10), <anonymous>:7:1)\\n    at Hook.CALL_DELEGATE [as _call] (/Users/marvin/code/marvhock.github.io/node_modules/tapable/lib/Hook.js:16:14)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5500:39\\n    at tryRunOrWebpackError (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/HookWebpackError.js:82:7)\\n    at __webpack_require_module__ (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5498:12)\\n    at __webpack_require__ (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5445:18)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5533:20\\n    at symbolIterator (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3485:9)\\n    at done (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3527:9)\\n    at Hook.eval [as callAsync] (eval at create (/Users/marvin/code/marvhock.github.io/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Hook.CALL_ASYNC_DELEGATE [as _callAsync] (/Users/marvin/code/marvhock.github.io/node_modules/tapable/lib/Hook.js:20:14)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5420:43\\n    at symbolIterator (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3482:9)\\n    at timesSync (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:2297:7)\\n    at Object.eachLimit (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3463:5)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5382:16\\n    at symbolIterator (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3485:9)\\n    at timesSync (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:2297:7)\\n    at Object.eachLimit (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3463:5)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5350:15\\n    at symbolIterator (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3485:9)\\n    at done (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3527:9)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5296:8\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:3696:6\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/HookWebpackError.js:68:2\\n    at Hook.eval [as callAsync] (eval at create (/Users/marvin/code/marvhock.github.io/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:15:1)\\n    at Cache.store (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Cache.js:113:20)\\n    at ItemCacheFacade.store (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/CacheFacade.js:142:15)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:3695:11\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Cache.js:97:34\\n    at Array.<anonymous> (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/cache/MemoryCachePlugin.js:46:13)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Cache.js:97:19\\n    at Hook.eval [as callAsync] (eval at create (/Users/marvin/code/marvhock.github.io/node_modules/tapable/lib/HookCodeFactory.js:33:10), <anonymous>:19:1)\\n    at Cache.get (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Cache.js:81:18)\\n    at ItemCacheFacade.get (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/CacheFacade.js:116:15)\\n    at Compilation._codeGenerationModule (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:3663:9)\\n    at codeGen (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5284:11)\\n    at symbolIterator (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3482:9)\\n    at timesSync (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:2297:7)\\n    at Object.eachLimit (/Users/marvin/code/marvhock.github.io/node_modules/neo-async/async.js:3463:5)\\n    at /Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/Compilation.js:5314:14\\n    at processQueue (/Users/marvin/code/marvhock.github.io/node_modules/webpack/lib/util/processAsyncTree.js:61:4)\\n    at process.processTicksAndRejections (node:internal/process/task_queues:85:11)\\n\\nGenerated code for /Users/marvin/code/marvhock.github.io/node_modules/css-loader/dist/cjs.js??ruleSet[1].rules[1].use[1]!/Users/marvin/code/marvhock.github.io/node_modules/postcss-loader/dist/cjs.js!/Users/marvin/code/marvhock.github.io/node_modules/sass-loader/dist/cjs.js!/Users/marvin/code/marvhock.github.io/src/css/main.scss\\n1 | throw new Error(\\\"Module build failed (from ./node_modules/sass-loader/dist/cjs.js):\\\\nexpected \\\\\\\"(\\\\\\\".\\\\n\\\\u001b[34m  ╷\\\\u001b[0m\\\\n\\\\u001b[34m9 │\\\\u001b[0m   @media (min-width: (settings.breakpoint-sm\\\\u001b[31m\\\\u001b[0m)) {\\\\n\\\\u001b[34m  │\\\\u001b[0m \\\\u001b[31m                                            ^\\\\u001b[0m\\\\n\\\\u001b[34m  ╵\\\\u001b[0m\\\\n  src/css/components/base.scss 9:45  @use\\\\n  src/css/main.scss 5:1              root stylesheet\\\");\");\n\n//# sourceURL=webpack://marvhock.com/./src/css/main.scss?\n}");
+
+/***/ }),
+
+/***/ "./src/js/main.js":
+/*!************************!*\
+  !*** ./src/js/main.js ***!
+  \************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+eval("{__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _css_main_scss__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./../css/main.scss */ \"./src/css/main.scss\");\n/* harmony import */ var _css_main_scss__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(_css_main_scss__WEBPACK_IMPORTED_MODULE_0__);\n\n\n//# sourceURL=webpack://marvhock.com/./src/js/main.js?\n}");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/compat get default export */
+/******/ 	(() => {
+/******/ 		// getDefaultExport function for compatibility with non-harmony modules
+/******/ 		__webpack_require__.n = (module) => {
+/******/ 			var getter = module && module.__esModule ?
+/******/ 				() => (module['default']) :
+/******/ 				() => (module);
+/******/ 			__webpack_require__.d(getter, { a: getter });
+/******/ 			return getter;
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	var __webpack_exports__ = __webpack_require__("./src/js/main.js");
+/******/ 	
+/******/ })()
+;
